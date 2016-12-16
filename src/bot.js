fcc06903b5225a2d3ea072e2852e48b34f9432bc
pure-js <evolution.is.already.underway@gmail.com>
2016-12-16 07:13:22 +0300

    add download from telegram

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,39 @@
-const TeleBot = require('telebot');
-const dropboxToken = require('../dropbox-token.js');
-const telegramToken = require('../telegram-token.js');
-const bot = new TeleBot(telegramToken);
+const TeleBot = require('telebot'),
+  fs = require('fs'),
+  dropboxToken = require('../dropbox-token.js'),
+  telegramToken = require('../telegram-token.js'),
+  bot = new TeleBot(telegramToken);
 
 const Dropbox = require('dropbox');
 let dbx = new Dropbox({ accessToken: dropboxToken });
-dbx.filesListFolder({path: ''})
+dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/0ifzbgkcpnl1vic/file%20%285%29.png?dl=0'})
   .then(function(response) {
-    console.log(response);
+    let imagedata = response.fileBinary;
+    const dir = './tmp';
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    fs.writeFile(dir + '/photo.png', imagedata, 'binary', function(err){
+      if (err) throw err
+      console.log('File saved.')
+    })
   })
   .catch(function(error) {
     console.log(error);
   });
 
-bot.on('text', msg => {
+bot.on('/start', msg => {
   let fromId = msg.from.id;
   let firstName = msg.from.first_name;
   let reply = msg.message_id;
   return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+});
+
+bot.on(['/captcha', '/c'], msg => {
+  let img = 'http://img.wikinut.com/img/gycf69_-6rv_5fol/jpeg/724x5000/Best-Friends-Img-Src%3AImage%3A-FreeDigitalPhotos.net.jpeg'
+  return bot.sendPhoto(msg.from.id, binary);
 });
 
 bot.connect();
